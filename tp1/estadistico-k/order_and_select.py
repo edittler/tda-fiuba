d65a105fcb395fd8dae55dcb3f1879c9fa4a8319
Ferno <fer.pereira9014@gmail.com>
2016-09-30 02:36:11 -0300

    Se agrega implementacion de quick_select, pruebas pertinentes y se elimina comentario

--- tp1/estadistico-k/order_and_select.py
+++ tp1/estadistico-k/order_and_select.py
@@ -7,9 +7,9 @@ def k_min(elements, k):
 
 
 def order_and_select(l, k):
-    """
-    Se ordena el conjunto dado y se devuelve el k-esimo valor
-    """
-    # TODO: No ser ladri e implementar un algoritmo de ordenamiento
-    list.sort(l)
-    return l[k]
+	"""
+	Se ordena el conjunto dado y se devuelve el k-esimo valor
+	"""
+
+	list.sort(l)
+	return l[k]
--- tp1/estadistico-k/tests.py
+++ tp1/estadistico-k/tests.py
@@ -7,6 +7,7 @@ import brute_force
 import order_and_select
 import k_selections
 import heap_select
+import quick_select
 
 
 one = [5, 2, 9, 7, 1, 90, 123, 8, 3]
@@ -47,13 +48,21 @@ def test_heap_select():
     l = copy.copy(one)
     assert heap_select.k_min(l, len(l) - 1) == 123
 
+def test_quick_select():
+	l = copy.copy(one)
+	assert quick_select.k_min(l, 3) == 5
+	l = copy.copy(one)
+	assert quick_select.k_min(l, 0) == 1
+	l = copy.copy(one)
+	assert quick_select.k_min(l, len(l) - 1) == 123
 
 def main(args):
-    # test_brute_force()
-    test_order_and_select()
-    test_k_selections()
-    test_heap_select()
-    return 0
+	# test_brute_force()
+	test_order_and_select()
+	test_k_selections()
+	test_heap_select()
+	test_quick_select()
+	return 0
 
 
 if __name__ == '__main__':
