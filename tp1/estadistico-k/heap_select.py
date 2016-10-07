0f14fd662560e91e9860ca7d7a150c2e257a59cb
Tomás Arjovsky <t.arjovsky@gmail.com>
2016-10-07 20:36:14 -0300

    heap_select con maxheap por tuplas

--- tp1/estadistico-k/heap_select.py
+++ tp1/estadistico-k/heap_select.py
@@ -7,17 +7,19 @@ import heapq
 def k_min(elements, k):
     return heap_select(elements.copy(), k)
 
-
 def heap_select(elements, k):
     """
     Se almancenan los k elementos más chicos en un heap de máximo y
     se devuelve el k-esimo valor
     """
-    heap = elements[:k+1]
-    heapq._heapify_max(heap)
+    minimos = [(-i, i) for i in elements[:k+1]]
+    heapq.heapify(minimos)
+
     for elem in elements[k+1:]:
         # Se inserta el elemento si y solo si es menor al máximo del heap.
         # En el caso de insertarlo, se saca el máximo.
-        heapq._heappushpop_max(heap, elem)
+        if elem < minimos[0][1]:
+            heapq.heappush(minimos, (-elem,elem))
+            heapq.heappop(minimos)
 
-    return heap[0]
+    return minimos[0][1]
